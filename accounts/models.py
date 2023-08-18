f72ffe43bf0e26fb48e8da17b570c9ae25b4bf27
KhusniddinovUz <khusniddinovuz@gmail.com>
2023-08-18 18:36:13 +0500

    Alter type of student year

--- accounts/models.py
+++ accounts/models.py
@@ -5,7 +5,7 @@ from django.contrib.auth.models import AbstractUser
 class User(AbstractUser):
     group_number = models.CharField(max_length=15, blank=True, null=True)
     student_id = models.CharField(max_length=20, blank=True, null=True)
-    student_year = models.IntegerField(blank=True, null=True)
+    student_year = models.CharField(max_length=1, blank=True, null=True)
 
     def __str__(self):
         return self.email
